d49004e3af006b1f52c4d6395a6bb7e178016343
Ramesha Gowda <ramesha.gowda@gmail.com>
2023-03-03 19:25:00 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create an ACR instance
 resource "azurerm_container_registry" "acr" {
-  name                     = "myacr"
+  name                     = "myacraksrandom"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Standard"
